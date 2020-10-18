5904fdf6cf6f8c167d7bc36837a525d218fd2f28
Ovidiu Cristurean <ovidiu.cristurean@endava.com>
2020-10-18 11:57:19 +0300

    small card view changes

--- fast_spectator/lib/ui/view/view_match_card.dart
+++ fast_spectator/lib/ui/view/view_match_card.dart
@@ -27,88 +27,91 @@ class MatchCardState extends State<MatchCardView> {
   @override
   Widget build(BuildContext context) {
     return Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(),
+        shadowColor: Colors.grey,
         child: Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  _matchInfo.title ?? "",
-                  textAlign: TextAlign.start,
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  _matchInfo.competition?.name ?? "",
-                  textAlign: TextAlign.end,
-                ),
-              )
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  _matchInfo.sideOne.name ?? "",
-                  textAlign: TextAlign.start,
-                ),
-              ),
-              Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.network(_matchInfo.thumbnail)),
-              Expanded(
-                child: Text(
-                  _matchInfo.sideTwo.name ?? "",
-                  textAlign: TextAlign.end,
-                ),
-              )
-            ],
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(left: 8),
-            ),
-            Expanded(
-              child: RaisedButton(
-                color: Colors.grey,
-                onPressed: () {
-                  navigateToDetail(_matchInfo.url);
-                },
-                elevation: 5.0,
-                child: Text("Match details"),
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _matchInfo.title ?? "",
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      _matchInfo.competition?.name ?? "",
+                      textAlign: TextAlign.end,
+                    ),
+                  )
+                ],
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(left: 8),
-            ),
-            Expanded(
-              child: RaisedButton(
-                color: Colors.grey,
-                onPressed: () {
-                  navigateToHighlights(_matchInfo.videos[0].embed);
-                },
-                elevation: 5.0,
-                child: Text("Check highlights"),
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _matchInfo.sideOne.name ?? "",
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Container(
+                      width: 100,
+                      height: 100,
+                      child: Image.network(_matchInfo.thumbnail)),
+                  Expanded(
+                    child: Text(
+                      _matchInfo.sideTwo.name ?? "",
+                      textAlign: TextAlign.end,
+                    ),
+                  )
+                ],
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 8),
-            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 8),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.grey,
+                    onPressed: () {
+                      navigateToDetail(_matchInfo.url);
+                    },
+                    elevation: 5.0,
+                    child: Text("Match details"),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.grey,
+                    onPressed: () {
+                      navigateToHighlights(_matchInfo.videos[0].embed);
+                    },
+                    elevation: 5.0,
+                    child: Text("Check highlights"),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8),
+                ),
+              ],
+            )
           ],
-        )
-      ],
-    ));
+        ));
   }
 
   void navigateToDetail(String url) async {
